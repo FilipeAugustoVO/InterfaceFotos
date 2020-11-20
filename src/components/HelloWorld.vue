5858ed87a1546501322fcb08ced31556bc0e4975
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2020-11-20 00:38:59 -0300

    Interface para Fotos
    Interface para fotos. Interface consume de um JSON externo. Incompleto, faltam favoritos, paginação e pesquisa.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div>
+    <h1>Teste</h1>
+    <ul>
+      <li :key="user.id" v-for="user in users">
+        <img :src="user.thumbnailUrl"/>
+      {{ user.title }}</li>
+    </ul>
+  </div>  
+</template>
+
+
+//Dois pontos faz o binding. Binding é dinâmico 
+
+<script>
+//import Axios
+import axios from 'axios'
+export default {
+  name: 'InterfaceFotos',
+  data: () => ({
+    users: []
+  }),
+  mounted () {
+    this.loadUsers()
+  },
+  methods: {
+    async loadUsers () {
+      try {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos')
+        console.log(data)
+        this.users = data
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+}
+
+
+</script>
+
+
---
+++ src/components/VuePaginator.vue
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      // The resource variable
+      animals: [],
+      // Here you define the url of your paginated API
+      resource_url: 'https://jsonplaceholder.typicode.com/photos'
+    }
+  },
+  components: {
+    VPaginator: VuePaginator
+  },
+  methods: {
+    updateResource(data){
+      this.animals = data
+    }
+  }
+  ...
+});
